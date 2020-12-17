54da80a1347e28a7aada7a9ebec4f5ed153894d9
Serega <borzunov_sm2mail.ru>
2020-12-17 17:48:28 +0300

    UILabel + UIImage + UITextView + UIScrollView

--- UIKITelements work/ViewController.swift
+++ UIKITelements work/ViewController.swift
@@ -8,10 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var teslaImageVire: UIImageView!
+    @IBOutlet weak var poemTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        label.text = "Функция \(#function)"
+        label.textColor = .red
+        teslaImageVire.image = UIImage(named: "Tesla")
+        if let text = poemTextView.text{
+            poemTextView.text = String(text.reversed())
+        }
+         
+        //UIScrollView
+        let imageView = UIImageView(image: UIImage(named:"Tesla"))
+        let scrollView = UIScrollView(frame: CGRect(x: view.bounds.midX - 50, y: 100, width: 100, height: 100))
+        scrollView.backgroundColor = .blue
+        scrollView.contentSize = imageView.bounds.size
+        scrollView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        scrollView.addSubview(imageView)
+        
+        view.addSubview(scrollView)
     }
 
 
